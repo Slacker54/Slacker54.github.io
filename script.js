2e63239b699d0c6fbccc22937a0cac4ef8f7ec22
Aaron Feia <acfeia@hotmail.com>
2019-03-27 19:52:49 -0500

    Make Website

---
+++ script.js
@@ -0,0 +1,15 @@
+// This is a function.
+function popup() {
+	alert("I'm the BOSS");
+}
+// This is a function.
+function doSomething() {
+	document.getElementById("field").style = "background:#0000FF";
+}
+
+// This is a function.
+function doSomethingElse() {
+	document.getElementById("field").style = "background:#FF0000";
+}
+
+
